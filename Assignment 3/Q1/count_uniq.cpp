94723053151703387c4f0e201ab9d75ddf466e35
Shyam Ramamoorthy <sr2897@gmail.com>
2016-02-15 22:41:12 +0530

    Adding STL map implementation to account for collisions

--- Assignment 3/Q1/count_uniq.cpp
+++ Assignment 3/Q1/count_uniq.cpp
@@ -1,12 +1,18 @@
+//
+//  count_uniq.cpp
+//
+//
+//  Created by Shyam Ramamoorthy on 15/02/16.
+//
+//  This implementation accounts for collisions.
+
 #include <iostream>
-#include <string>
-#include <set>
+#include <vector>
+#include <map>
 #include "mfile.hpp"
 
-#define A 54059
-#define B 76963 
-#define C 86969 
-
+#define A 54059 /* a prime */
+#define B 76963 /* another prime */
 unsigned Hasher(const std::string& s)
 {
     unsigned int h = 31 ;
@@ -15,10 +21,15 @@ unsigned Hasher(const std::string& s)
     {
         h = (h * A) ^ (s[i] * B);
     }
-    return h; 
+    return h;
 }
 
+
 int main(){
+    std::map <int, vector<int> > store;
+    int uniq = 0;
+    int index = 0;
+    
     std::string inp_file;
     std::cin >> inp_file;
     
@@ -26,19 +37,43 @@ int main(){
     std::cin >> n;
     
     MFile file;
-    file.init(inp_file,n);
+    file.init(inp_file, n);
     
-    std::string current = file.readNext();
-    std::set<int> store;
-    int uniq=0;
+    std::string line = file.readNext();
     
-    
-    while(current.length() > 0) {
-        int cur_hash = Hasher(current);
-        if(store.insert(cur_hash).second) uniq += 1;
-        current = file.readNext();
+    while (!line.empty()){
+        int line_hash = Hasher(line);
+        
+        std::vector<int> cur_line;
+        cur_line.push_back(index);
+        
+        if(store.insert(pair<int, vector<int> > (line_hash, cur_line)).second) uniq += 1;
+        //When the current hash doesn't match with any of the previous ones.
+        
+        else{
+            //When the current hash already exists in the map.
+            std::vector<int> line_nums = (store.find(line_hash))->second;
+            
+            std::vector<string> prev_lines = file.readLines (line_nums);
+            bool isUnique = true;
+            
+            for (std::vector<string>::iterator it = prev_lines.begin() ; it != prev_lines.end(); ++it){
+                if(*it == line) {
+                    isUnique = false;
+                    break;
+                }
+            }
+            
+            if (isUnique){
+                uniq += 1;
+                (store.find(line_hash)->second).push_back(index);
+            }
+            
+
+        }
+        index += 1;
+        line = file.readNext();
     }
-    
-   std::cout << uniq;
-    
+    file.close();
+    std::cout << uniq << endl;
 }
